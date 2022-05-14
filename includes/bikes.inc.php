f5a8e7ee2b81f6ab1cdc593027259e49c503d302
George Kanaridis <89773658+BornToBeCool@users.noreply.github.com>
2022-05-14 01:21:13 +0100

    Update bikes.inc.php

--- includes/bikes.inc.php
+++ includes/bikes.inc.php
@@ -1,3 +1,7 @@
+<?php
+    include "includes/indexhead.inc.php";
+    include "includes/header.inc.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,55 +18,6 @@
     <title> Bike King Borders - Login </title>
 </head>
 <body>
-    <header>
-        <div class="container">
-            <div class="top_header">
-                <img src="/images/Logo_main.png"  class="logo" alt="Logo">
-                <h3  class="brandName">BIKE KING BORDERS</h3>
-                <button class="hamburger" id="hamburger" onclick="hide()">
-                    <i class="fas fa-bars"></i>
-                </button>
-                <img src="/images/Phone.png"  class="phoneIcon" alt="Phone">
-                <h3 class="phoneNumber">0141 076 482</h3>
-            </div>
-            <div class="mid_header">
-                <a href="#" class="contactPage">Contact</a>
-                <a href="/pages/logIn.html" class="loginPage">Log In</a>
-            </div>
-            <div class="bot_header">
-                <input type="text" name="" placeholder="     Type to search" class="searchBox" id="searchBox">
-                <div class="search_box" id="search_box">
-                    <img src="/images/search.png" class="searchIcon" id="searchIcon">
-                </div>
-                <div class="icons">
-                    <a href="/pages/logIn.html"><i class="fa fa-user"></i></a>
-                    <a href="#"><img src="/images/Basket.png" class="basketIcon" id="basketIcon"></a>
-                </div>
-            </div>
-            <nav class="navBar">
-                <ul class="nav_bar" id="navBar">
-                    <li><a href="/index.html" class="list_items">HOME</a></li>
-                    <div class="dropdown">
-                        <li><a href="#"  class="list_items">BIKES</a></li>
-                        <div class="drop-down-content">
-                            <li><a href="/pages/mountain.html"  class="list_items">MOUNTAIN</a></li>
-                            <li><a href="/pages/hybrid.html"  class="list_items">  HYBRID</a></li>
-                            <li><a href="/pages/road.html"  class="list_items">    ROAD</a></li>
-                            <li><a href="/pages/kids.html"  class="list_items">    KIDS</a></li>
-                        </div>
-                    </div>
-                    <li><a href="#"  class="list_items">CLOTHING</a></li>
-                    <li><a href="#"  class="list_items">TRAILS</a></li>
-                    <li><a href="#"  class="list_items">ACCESSORIES</a></li>
-                    <li><a href="#"  class="list_items">REPAIRS</a></li>
-                    <li><a href="#"  class="list_items">HIRE</a></li>
-                    <li><a href="#"  class="list_items">ABOUT</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-    
-
     <div class="mountainBikes">Mountain Bikes</div>
     <section class="main-container">
         <div class="products">
